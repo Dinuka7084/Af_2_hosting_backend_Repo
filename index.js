5f4f0ccd966a273147ff4d2a39fc35d963fe9a88
DLK624 <165545205+DLK624@users.noreply.github.com>
2025-05-06 13:17:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use(cors({
     origin:`http://localhost:5173`,
     credentials: true
 }))
+app.get('/', (req, res) => {
+  res.send('Backend is live!');
+});
 
 app.use('/api', authRoutes);
 app.use('/api', favouriteRoutes);
